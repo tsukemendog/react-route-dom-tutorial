773eaf46e4b3bebd0559f00d7d2d873e0487035d
mokaim <mokaim@outlook.com>
2023-11-02 23:06:34 +0900

    예제 구현완료

--- src/main.jsx
+++ src/main.jsx
@@ -8,8 +8,12 @@ import "./index.css";
 
 import Root, { loader as rootLoader,   action as rootAction, } from "./routes/root";
 import ErrorPage from "./error-page";
-import Contact, {loader as contactLoader} from "./routes/contact";
+import Contact, {loader as contactLoader, action as contactAction,} from "./routes/contact";
 import EditContact, {action as editAction,} from "./routes/edit";
+import Index from "./index.jsx";
+
+import { action as destroyAction } from "./routes/destroy";
+
 
 const router = createBrowserRouter([
   {
@@ -20,15 +24,38 @@ const router = createBrowserRouter([
     action: rootAction,
     children: [
       {
+        index: true,
+        element: <Index />
+      },
+      {
+        errorElement: <ErrorPage />,
+        children: [
+          { index: true, element: <Index /> },
+          {
+            path: "contacts/:contactId",
+            element: <Contact />,
+            loader: contactLoader,
+            action: contactAction,
+          },
+          /* the rest of the routes */
+        ],
+      },
+/*      {
         path: "contacts/:contactId",
         element: <Contact />,
         loader: contactLoader,
-      },
+        action: contactAction,
+      },*/
       {
         path: "contacts/:contactId/edit",
         element: <EditContact />,
         loader: contactLoader,
         action: editAction,
+      },
+      {
+        path: "contacts/:contactId/destroy",
+        action: destroyAction,
+        errorElement: <div>Oops!! There was an error.</div>
       },
     ],
   },
